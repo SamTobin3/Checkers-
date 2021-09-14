cf67a9a99aeeb78b7bd655e12878e004ccaabbf0
Sam Tobin <73917370+SamTobin3@users.noreply.github.com>
2021-09-14 13:48:11 -0230

    Add files via upload

---
+++ Java-Checkers/src/model/HumanPlayer.java
@@ -0,0 +1,17 @@
+
+
+package model;
+
+
+public class HumanPlayer extends Player {
+
+	@Override
+	public boolean isHuman() {
+		return true;
+	}
+
+	
+	@Override
+	public void updateGame(Game game) {}
+
+}
